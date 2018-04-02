78990aab70d85558d9696bf0914360f20cf77197
Matthew <furikuribeebop@gmail.com>
2018-04-02 12:27:15 +0930

    style changes and inclusion of Sign in Modal
    Experimenting with different stylings and setups, currently part way through sign in modal

--- homepage.js
+++ homepage.js
@@ -3,6 +3,9 @@ function createHamburger(x) {
     x.classList.toggle("change");
 }
 
+//#################################################
+//for simulation of a loggd in user with menu styles
+
 function logOut() {
     console.log("pressed log out");
 
@@ -18,6 +21,8 @@ function logIn() {
     $(".loggedInWelcome").css("display", "block")
 }
 
+//#####################################################
+
 var visible = false; //Is the menu currently visible?
 function menuChange() {
     if(visible) {
@@ -28,5 +33,25 @@ function menuChange() {
         visible = true;
     }
 }
-    
+
+
+//display sign in form on click
+$( ".signInUp" ).click(function() {
+    $(".modal").show();
+  });
+
+//hide sign in form on click
+$( ".closeBtn" ).click(function() {
+    $(".modal").hide();
+  });
+
+//hide on any click outside of modal
+$(document).click(function(event) {
+//if you click on anything except the modal itself or the "open modal" link, close the modal
+if (!$(event.target).closest(".modal-content, .signInUp").length) {
+    $("body").find(".modal").hide();
+}
+});
+  
+
 
