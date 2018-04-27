36922e2b1471b2bf0c9ee9388b701d620d65c09f
Matthew <furikuribeebop@gmail.com>
2018-04-27 20:25:33 +0930

    fixed homepage links

--- server-site/routes/index.js
+++ server-site/routes/index.js
@@ -12,7 +12,7 @@ router.set("view engine", "ejs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('landing');
+  res.render('landing', {hotels:hotels, recommended:recommended, popular:popular, highestRated: highestRated});
 });
 
 /* GET search page. */
@@ -84,6 +84,9 @@ router.post('/search', function(req, res, next) {
 //these are needed to fill out all the pages, later on (in submssion 4 i think, this will all be transferred to a database)
 //but for now i gues they can go here, no idea where else to stick them atm
 var hotels = [];
+var recommended = [];
+var popular = [];
+var highestRated = [];
 
 //rooms array - dont need this at the moment
 
@@ -121,20 +124,45 @@ var hotelTest2 = new Hotel("Kratos' shack", "Norway", 35.6897, 139.6917);
 var hotelTest3 = new Hotel("Kaer Morhen", "Poland?", 35.6899, 139.6917);
 var hotelTest4 = new Hotel("yorHa Bunker", "Orbit", 35.6895, 139.6915);
 var hotelTest5 = new Hotel("Baggins Hobbit Hole", "The Shire", 35.6895, 139.6913);
+var hotelTest6 = new Hotel("Konoha", "Kyoto", 35.0116, 135.7680);
+var hotelTest7 = new Hotel("Konoha", "Kyoto", 35.0116, 136.7680);
+var hotelTest8 = new Hotel("Hanshin Tigers Den", "Osaka", 34.7213, 135.3616);
+var hotelTest9 = new Hotel("Nagano Nest", "Kyoto", 36.6486, 138.1948);
+var hotelTest10 = new Hotel("Hakuba 47", "Kyoto", 36.6982, 137.8619);
+
 hotels.push(hotelTest);
+recommended.push(hotelTest);
 hotels.push(hotelTest2);
+recommended.push(hotelTest2);
 hotels.push(hotelTest3);
+recommended.push(hotelTest3);
 hotels.push(hotelTest4);
+popular.push(hotelTest4);
 hotels.push(hotelTest5);
+popular.push(hotelTest5);
+hotels.push(hotelTest6);
+popular.push(hotelTest6);
+hotels.push(hotelTest7);
+highestRated.push(hotelTest7);
+hotels.push(hotelTest8);
+highestRated.push(hotelTest8);
+hotels.push(hotelTest9);
+highestRated.push(hotelTest9);
+hotels.push(hotelTest10);
 
 //dummy room 
 var roomTest1 = new Room("Pod 1", "all of them for now", "over 9000!", "alot");
 roomTest1.images[0] = "pup.jpg";
 roomTest1.images[1] = "kabukicho.jpg";
 
+var roomTest2 = new Room("Pod 2", "all of them for now", "over 20,000!", "alot");
+roomTest2.images[0] = "pup.jpg";
+roomTest2.images[1] = "kabukicho.jpg";
+
 //put the room into the room array of each hotel
 hotels.forEach(function(hotel){
   hotel.rooms.push(roomTest1);
+  hotel.rooms.push(roomTest2);
 });
 
 console.log(hotels);
